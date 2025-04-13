e21e0e0f96e5b4929a6220a0fc845d4daea07ba8
amoskala-wsb <wrx91039@student.wroclaw.merito.pl>
2025-04-13 17:01:06 +0200

    Test1

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Adam"
+moje_imie = "Adam Moskała"
 msg = "Hello World!"
 
 @app.route('/')
